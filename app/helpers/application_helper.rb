7dfd8a5b03b5ddc0f47d12b2f0fb9597ae26b4bd
Cameron Walters <cameron.walters@gmail.com>
2008-08-19 00:27:07 -0700

    Add inspiration, minor formatting

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,16 @@ module ApplicationHelper
   def analytics
     render :file => 'layouts/analytics.html' if AnalyticsEnvironments.include? Rails.env
   end
+  
+  def colored_google
+    '<b class="g">G</b><b class="o1">o</b><b class="o2">o</b><b class="g">g</b><b class="l">l</b><b class="e">e</b>'
+  end
+  
+  def blur_links
+    javascript_tag do <<-JS
+        var links = document.getElementsByTagName('a');
+        for (var i = links.length - 1; i >= 0; i--){ var link = links[i]; link.onclick = function() { link.blur(); } };
+      JS
+    end
+  end
 end
