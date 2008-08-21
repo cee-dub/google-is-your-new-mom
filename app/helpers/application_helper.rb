6f61693a53f063a47a10e96b80623ede0022cff4
Cameron Walters <cameron.walters@gmail.com>
2008-08-21 11:52:36 -0700

    Add analytics on deploy.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,5 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
-  
-  AnalyticsEnvironments = Set.new %w( production express )
-  def analytics
-    render :file => 'layouts/analytics.html' if AnalyticsEnvironments.include? Rails.env
-  end
-  
   def colored_google
     '<b class="g">G</b><b class="o1">o</b><b class="o2">o</b><b class="g">g</b><b class="l">l</b><b class="e">e</b>'
   end
